dd3bb2093057895e7667e43dac76d6e93a77fa0c
shoty-de <66251668+shoty-de@users.noreply.github.com>
2021-06-23 15:37:57 +0900

    Update users_controller.rb
    loginアクション編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,13 @@ class UsersController < ApplicationController
   end
 
   def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      flash[:notice] = "ログインしました"
+      redirect_to("/home")
+    else
+      render("users/login_form")
+    end
   end
 
 end
