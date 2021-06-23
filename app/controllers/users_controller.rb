43c482acd1042ef3d2e983ac34c6116c2cd3d205
shoty-de <66251668+shoty-de@users.noreply.github.com>
2021-06-23 17:08:25 +0900

    add
    ログインユーザーの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,7 @@ class UsersController < ApplicationController
   def login
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to("/home")
     else
