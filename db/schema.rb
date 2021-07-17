d6d66a542a55bfe9c37e129310343cbdd50dcc86
shoty-de <66251668+shoty-de@users.noreply.github.com>
2021-07-17 18:18:09 +0900

    add
    カラム追加、マイグレーションファイル編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_034854) do
+ActiveRecord::Schema.define(version: 2021_07_14_131447) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_06_23_034854) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password"
+    t.string "image_name"
   end
 
 end
