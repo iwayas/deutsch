036e4c32ddef563d6e812b6accb88f0cbcf3909f
shoty-de <66251668+shoty-de@users.noreply.github.com>
2021-08-08 22:10:04 +0900

    add
    ページネーション

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(6)
   end
 
   def show
