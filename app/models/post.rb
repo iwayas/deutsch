5f90ee56f116363ccf8348c1529fb79224abbc05
shoty-de <66251668+shoty-de@users.noreply.github.com>
2021-08-08 22:45:56 +0900

    add
    placeholder

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :content, {presence: true, length: {maximum: 500}}
+  validates :content, {presence: true, length: {maximum: 140}}
   validates :user_id, {presence: true}
 
   def user
