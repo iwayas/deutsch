ed76a96c96a13133886f07984e12a4f357774415
shoty-de <66251668+shoty-de@users.noreply.github.com>
2021-10-06 15:49:48 +0900

    add
    capistrano導入

---
+++ config/deploy.rb
@@ -0,0 +1,31 @@
+lock "~> 3.14.1"
+set :application, 'deutsch'
+set :repo_url, "git@github.com:shoty-de/deutsch.git"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/deploytest.pem']
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+set :linked_files, %w{ config/secrets.yml }
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'uunicorn:restart'
+  end
+end
+
+desc 'upload secrets.yml'
+task :upload do
+  on roles(:app) do |host|
+    if test "[ ! -d #{shared_path}/config ]"
+      execute "mkdir -p #{shared_path}/config"
+    end
+    upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+  end
+end
+before :starting, 'deploy:upload'
+after :finishing, 'deploy:cleanup'
+end
