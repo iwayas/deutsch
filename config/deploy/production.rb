04e49b8626cc26b7eb9789ac2d0d9cc286290251
shoty-de <66251668+shoty-de@users.noreply.github.com>
2021-10-20 16:31:29 +0900

    change
    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '35.76.241.103', user: 'ec2-user', roles: %w{app db web}
+server '18.180.76.171', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
